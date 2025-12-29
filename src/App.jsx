aaed9d7ab372a9c3b5c72dcf194006507895804d
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2025-12-29 11:09:46 +0500

    Complete Ex-6k create not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { OrderPage } from './pages/OrderPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Routes, Route } from 'react-router'
+import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrderPage />} />
       <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
     
   )
