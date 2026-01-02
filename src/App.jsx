51e8487023569c5ee068187fb28c97e9349954d6
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-02 10:07:14 +0500

    Complete Ex-7o update the not found page

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,11 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage 
+      cart={cart} />} />
       <Route path="orders" element={<OrderPage cart={cart}/>} />
       <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage cart={cart} />} />
     </Routes>
     
   )
