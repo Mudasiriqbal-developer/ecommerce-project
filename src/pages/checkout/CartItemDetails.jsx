8476f35b9f4d3786edc9f9530c13c203d2f8d1a5
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-03 21:48:58 +0500

    Complete Ex-8e & 8f move cartDetails into own files

---
+++ src/pages/checkout/CartItemDetails.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useState } from "react";
+import { formatMoney } from "../utils/money";
+
+export function CartItemDetails({cartItem, laodCart}) {
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+
+  // function for to delete items from the cart.
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`)
+    await laodCart();
+  };
+
+  const updateQuantity = () => {
+    // Switch between true and false for isUpdatingQuantity.
+    if (isUpdatingQuantity) {
+      setIsUpdatingQuantity(false);
+    } else {
+      setIsUpdatingQuantity(true);
+    }
+  };
+
+  return (
+    <>
+            <img className="product-image" src={cartItem.product.image} />
+
+        <div className="cart-item-details">
+          <div className="product-name">{cartItem.product.name}</div>
+          <div className="product-price">
+            ${formatMoney(cartItem.product.priceCents)}
+          </div>
+          <div className="product-quantity">
+            <span>
+              Quantity: {isUpdatingQuantity
+              ? <input type="text" className="quantity-textbox" />
+              : <span className="quantity-label">{cartItem.quantity}
+              </span>
+            }
+            </span>
+            <span className="update-quantity-link link-primary"
+              onClick={updateQuantity}
+            >
+              Update
+            </span>
+            <span className="delete-quantity-link link-primary"
+             onClick={deleteCartItem}>
+              Delete
+            </span>
+          </div>
+        </div>
+    </>
+  );
+}
--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,9 +1,10 @@
 import dayjs from "dayjs";
-import axios from "axios";
-import { formatMoney } from "../utils/money";
+import { useState } from "react";
 import { DeliveryOptions } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
 
-export function OrderSummary({cart, deliveryOptions, laodCart}) {
+export function OrderSummary({ cart, deliveryOptions, laodCart }) {
+
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -13,12 +14,6 @@ export function OrderSummary({cart, deliveryOptions, laodCart}) {
               return deliveryOption.id === cartItem.deliveryOptionId;
             }
           );
-
-          // function for to delete items from the cart.
-          const deleteCartItem = async () => {
-            await axios.delete(`/api/cart-items/${cartItem.productId}`)
-            await laodCart();
-          }
 
           return (
             <div key={cartItem.productId} className="cart-item-container">
@@ -30,36 +25,16 @@ export function OrderSummary({cart, deliveryOptions, laodCart}) {
               </div>
 
               <div className="cart-item-details-grid">
-                <img className="product-image" src={cartItem.product.image} />
-
-                <div className="cart-item-details">
-                  <div className="product-name">{cartItem.product.name}</div>
-                  <div className="product-price">
-                    ${formatMoney(cartItem.product.priceCents)}
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity:{" "}
-                      <span className="quantity-label">
-                        {cartItem.quantity}
-                      </span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary"
-                    onClick={deleteCartItem}>
-                      Delete
-                    </span>
-                  </div>
-                </div>
-
-                <DeliveryOptions 
+                <CartItemDetails 
                   cartItem={cartItem} 
-                  deliveryOptions={deliveryOptions} 
+                  laodCart={laodCart} 
+                />
+
+                <DeliveryOptions
+                  cartItem={cartItem}
+                  deliveryOptions={deliveryOptions}
                   laodCart={laodCart}
                 />
-                
               </div>
             </div>
           );
