05682a039fe49417ccf85eb3cf6bffb197c22543
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-04 20:00:55 +0500

    Test clicking add to cart button

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,9 +1,13 @@
 import { it, expect, describe, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
 import { Product } from './Product';
 
+vi.mock('axios');
+
 describe('Product Component', () => {
-  it('displays the product detail correctly', () => {
+  it('displays the product detail correctly', async () => {
     const product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -20,9 +24,18 @@ describe('Product Component', () => {
 
     render(<Product product={product} loadCart={loadCart}/> );
 
-    expect(
-      screen.getByText('$10.90')
-    ).toBeInTheDocument();
     
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId('add-to-cart-button')
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1
+      }
+    )
+    expect(loadCart).toHaveBeenCalled();
   });
 });
