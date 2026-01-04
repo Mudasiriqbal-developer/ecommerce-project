190ba6cb841734670fa965f09489298c74bf1d68
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-04 08:27:07 +0500

    Complete Ex-8j make search bar workable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router';
+import { useState } from 'react';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
@@ -6,6 +7,15 @@ import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './header.css';
 
 export function Header({cart}) {
+    const [search, setSearch] = useState('');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    console.log(search);
+  };
 
   let totalQuantity = 0;
 
@@ -24,9 +34,11 @@ export function Header({cart}) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar" type="text" placeholder="Search"
+          value={search} onChange={updateSearchInput} />
 
-        <button className="search-button">
+        <button className="search-button"
+          onClick={searchProducts}>
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
