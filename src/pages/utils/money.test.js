5aaaa86fa918d6b260c97427b34d505fb3011e36
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-05 10:17:27 +0500

    Check zeros in money formate

--- src/pages/utils/money.test.js
+++ src/pages/utils/money.test.js
@@ -10,5 +10,10 @@ it('Display 2 decimal', () => {
   expect(formatMoney(1090)).toBe('$10.90');
   expect(formatMoney(100)).toBe('$1.00');
 });
+
+it('Display Zeros 0', () => {
+  expect(formatMoney(0)).toBe('$0.00');
+});
+
 })
 
